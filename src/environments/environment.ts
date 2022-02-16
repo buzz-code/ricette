31188c6b6e619844b0f127e784b146ef562d736a
Hadasa Schechter <72219643+buzz-code@users.noreply.github.com>
2022-02-16 08:19:01 +0200

    add environemnt file

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export default {
+    production: process.env.mode === 'production',
+    firebase: {
+        apiKey: process.env.firebaseApiKey,
+        authDomain: process.env.firebaseAuthDomain,
+        databaseURL: process.env.firebaseDatabaseUrl,
+        projectId: process.env.firebaseProjectId,
+        storageBucket: process.env.firebaseStorageBucket,
+        messagingSenderId: process.env.firebaseMessagingSenderId,
+        appId: process.env.firebaseAppId,
+        measurementId: process.env.firebaseMeasurementId
+    }
+};
