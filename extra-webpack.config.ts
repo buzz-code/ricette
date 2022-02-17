9c5ec60f52d5d7726ef32535b6d9a1dee89a5479
Hadasa Schechter <72219643+buzz-code@users.noreply.github.com>
2022-02-17 15:37:17 +0200

    fix typo

--- extra-webpack.config.ts
+++ extra-webpack.config.ts
@@ -11,7 +11,7 @@ export default {
             'firebaseStorageBucket',
             'firebaseMessagingSenderId',
             'firebaseAppId',
-            'firebaseMeasurementI',
+            'firebaseMeasurementId',
         ])
     ]
 } as webpack.Configuration;
